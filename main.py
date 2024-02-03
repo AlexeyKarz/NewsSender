8d198044a44f9232c3334e6085f80a32f0ca1c95
alexeyks <karzanovalexey@gmail.com>
2024-02-03 17:50:15 +0200

    add the base application code from the course

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import yagmail
+import pandas
+from news import NewsFeed
+import datetime
+import time
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def send_email():
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date=yesterday,
+                         to_date=today)
+    email = yagmail.SMTP(user="YOUR GMAIL ADDRESS", password="PASSWORD OF YOUR GMAIL ADDRESS")
+    email.send(to=row['email'],
+               subject=f"Your {row['interest']} news for today!",
+               contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. \n{news_feed.get()}\nArdit")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+while True:
+    if datetime.datetime.now().hour == 13 and datetime.datetime.now().minute == 28:
+        df = pandas.read_excel('people.xlsx')
+
+        for index, row in df.iterrows():
+            send_email()
+
+    time.sleep(60)
